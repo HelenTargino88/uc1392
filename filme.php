263a5080fb4bbb74aedda67ba37c51cecb12e65c
Helen Nascimento Targino <helenntargino@gmail.com>
2022-11-17 11:57:51 -0300

    consultasql

--- filme.php
+++ filme.php
@@ -2,14 +2,14 @@
 include 'conecta.php';
 
 // cria a consulta sql
-$consulta = "select * from filme";
+$consultaSql = "select * from filme";
 
 // trazer a lista completa dos dados
-$lista = $pdo->query($consulta);
+$lista = $pdo->query($consultaSql);
 
 // separar os dados em linhas
-$linha = $lista->fetch();
-$num_linhas = $lista->rowCount();
+$row = $lista->fetch();
+$num_rows = $lista->rowCount();
 
 // echo 'A consulta retornou <strong>'.$num_linhas.'</strong> funcionarios <br>';
 // // print_r($linha);
@@ -17,6 +17,23 @@ $num_linhas = $lista->rowCount();
 // do{
 //     echo $linha['nome'].' - '.$linha['cpf'].'<br>';
 // } while($linha = $lista->fetch());
+
+if(isset($_POST['enviar']))
+{
+    $titulo = $_POST['titulo'];
+    $sinopse = $_POST['sinopse'];
+    $lancamento = $_POST['lancamento'];
+    $pais_origem = $_POST['pais_origem'];
+    $duracao = $_POST['duracao'];
+    $preco = $_POST['preco'];
+    $cod_classificacao = $_POST['cod-classificacao'];
+    $consulta = "insert filme (titulo,sinopse,lancamento,pais_origem, duracao,preco,cod_classificacao) values ('$titulo','$sinopse','$lancamento','$pais_origem','$duracao','$preco','$cod_classificacao')";
+    $resultado = $pdo->query($consulta);
+    $_POST['enviar'] = null;
+    header('location: filme.php');
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -24,6 +41,7 @@ $num_linhas = $lista->rowCount();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmes <?php echo '('.$num_linhas.')' ?></title>
+    <link rel="stylesheet" href="css/style.css">
     <style>
         td{
             border-bottom: 1px solid purple;
@@ -31,22 +49,85 @@ $num_linhas = $lista->rowCount();
     </style>
 </head>
 <body>
+    <section class="formulario">
+        <form action="#" method="post">
+            <div hidden>
+                <label for="cod-filme">
+                    Código
+                    <input type="text" name="cod-filme">
+                </label>
+            </div>
+            <div>
+                <label for="titulo">
+                    Titulo
+                <input type="textarea" name="titulo" required>
+                </label>
+            </div>
+            <div>
+                <label for="sinopse">
+                    Sinopse
+                <input type="text" name="sinopse">
+                </label>
+            </div>
+            <div>
+                <label for="lancamento">
+                    Lançamento
+                <input type="text" name="lancamento">
+                </label>
+            </div>
+            <div>
+                <label for="pais_origem">
+                    País de Origem
+                <input type="text" name="pais_origem">
+                </label>
+            </div>
+            <div>
+                <label for="duracao">
+                    Duração
+                <input type="text" name="duracao">
+                </label>
+            </div>
+            <div>
+                <label for="preco">
+                    Preço
+                <input type="text" name="preco">
+                </label>
+            </div>
+            <div>
+                <label for="cod-classificacao">
+                    Classificação Indicativa
+                <input type="text" name="cod-classificacao">
+                </label>
+            </div>
+            <div>
+                <button type="submit" name="enviar">Enviar</button>
+            </div>
+        </form>
+    </section>
     <table>
         <thead>
-            <th>ID</th>
+            <th>Cod</th>
             <th>Título</th>
             <th>Sinopse</th>
             <th>Lançamento</th>
+            <th>País de Origem</th>
+            <th>Duração</th>
+            <th>Preço</th>
+            <th>Classificação</th>
         </thead>
         <tbody>
             <?php do { ?>
                 <tr>                    
-                    <td><?php echo $linha['cod_filme']?></td>
-                    <td><?php echo $linha['titulo']?></td>
-                    <td><?php echo $linha['sinopse']?></td>
-                    <td><?php echo $linha['lancamento']?></td>
+                    <td><?php echo $row['cod_filme'];?></td>
+                    <td><?php echo $row['titulo'];?></td>
+                    <td><?php echo $row['sinopse'];?></td>
+                    <td><?php echo $row['lancamento'];?></td>
+                    <td><?php echo $row['pais_origem'];?></td>
+                    <td><?php echo $row['duracao'];?></td>
+                    <td><?php echo $row['preco'];?></td>
+                    <td><?php echo $row['cod_classificacao'];?></td>
                 </tr>
-            <?php } while ($linha = $lista->fetch());?>
+            <?php } while ($row = $lista->fetch())?>
         </tbody>
     </table>
 </body>
